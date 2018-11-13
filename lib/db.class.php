75c06b39931e1e98aa03bb51b42f46e4f5061cdf
lest4t <itlest4t@gmail.com>
2018-11-13 17:40:29 +0500

    append query funciton to DB class

--- lib/db.class.php
+++ lib/db.class.php
@@ -17,6 +17,32 @@ class DB
 		}
 	}
 
+	public function query($sql, $isAdd = false) {
+		if (!$this->connection) {
+			return false;
+		}
+
+		$result = $this->connection->query($sql);
+
+		if (mysqli_error($this->connection)) {
+			throw new Exception(mysqli_error($this->connection));
+		}
+
+		if ($isAdd) {
+			return mysqli_insert_id($this->connection);
+		}
+		if (is_bool($result)) {
+			return $result;
+		}
+
+		$data = array();
+		while ($row = mysqli_fetch_assoc($result)) {
+			$data[] = $row;
+		}
+
+		return $data;
+	}
+
 	public function escape($str) {
 		return mysqli_escape_string($this->connection, $str);
 	}
---
+++ models/Tests.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Date: 11/13/18
+ * Time: 5:31 PM
+ * Author: lest4t
+ */
