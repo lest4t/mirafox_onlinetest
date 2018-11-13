aaf32be31ba0de71894221ed4eee91520145cb9c
lest4t <itlest4t@gmail.com>
2018-11-13 19:15:00 +0500

    append default UI

--- views/default.php
+++ views/default.php
@@ -13,6 +13,8 @@
 		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="/css/styles.css">
+	<script type="text/javascript" src="/js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="/js/scripts.js"></script>
 	<title><?= Config::get('site_name'); ?></title>
 </head>
 <body>
--- views/index/index.php
+++ views/index/index.php
@@ -5,3 +5,17 @@
  * Author: lest4t
  */
 ?>
+
+<div class="tab">
+	<button class="tablinks" onclick="showTab(event, 'testing')">Тестирование</button>
+	<button class="tablinks" onclick="showTab(event, 'results')">Результаты</button>
+</div>
+
+<div id="testing" class="tabcontent">
+	<h3>Тестирование</h3>
+	<h4>Настройки тестирования</h4>
+</div>
+
+<div id="results" class="tabcontent">
+	<h3>Результаты</h3>
+</div>
