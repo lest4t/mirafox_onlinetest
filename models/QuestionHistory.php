1cc47f9068bd48dceee53cc6ac2d82d95f7714fb
lest4t <itlest4t@gmail.com>
2018-11-14 00:20:49 +0500

    added model for table questions_history

---
+++ models/QuestionHistory.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Date: 11/13/18
+ * Time: 11:55 PM
+ * Author: lest4t
+ */
+
+class QuestionHistory extends Model
+{
+	static $table = 'question_history';
+}
---
+++ models/Questions.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Date: 11/13/18
+ * Time: 8:34 PM
+ * Author: lest4t
+ */
