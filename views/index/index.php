c5444360676e441a1f4e636e5dd5becb708527bc
lest4t <itlest4t@gmail.com>
2018-11-13 20:10:03 +0500

    added else UI

--- views/index/index.php
+++ views/index/index.php
@@ -12,10 +12,45 @@
 </div>
 
 <div id="testing" class="tabcontent">
-	<h3>Тестирование</h3>
-	<h4>Настройки тестирования</h4>
+	<h4><b>Настройки тестирования</b></h4>
+	<form id="form_testing">
+		Сложность:
+		<label for="difficult_from">от</label>
+		<input type="number" min="0" max="100" name="difficult_from" id="difficult_from" value="0" />
+		<label for="difficult_to">до</label>
+		<input type="number" min="0" max="100" name="difficult_to" id="difficult_to" value="100" /><br>
+		<label for="person_iq">Интелект тестируемого:</label>
+		<input type="number" min="0" max="100" name="person_iq" id="person_iq" value="50" /><br>
+		<button type="button" id="generate_test">Запустить</button>
+	</form>
+	<br>
+	<table class="results">
+		<thead>
+		<tr>
+			<td>№</td>
+			<td>ID вопроса по БД</td>
+			<td>Использовался</td>
+			<td>Сложность</td>
+			<td>Верный ответ (Да/Нет)</td>
+		</tr>
+		</thead>
+		<tbody id="test_result"></tbody>
+	</table>
+	<div id="test_result_count"></div>
 </div>
 
 <div id="results" class="tabcontent">
-	<h3>Результаты</h3>
+	<h4><b>Результаты</b></h4>
+	<table class="results">
+		<thead>
+		<tr>
+			<td>№</td>
+			<td>Интелект тестируемого</td>
+			<td>Диапазон сложности вопросов</td>
+			<td>Результат тестирования</td>
+		</tr>
+		</thead>
+		<tbody></tbody>
+	</table>
+	<button type="button" id="refresh">Обновить</button>
 </div>
